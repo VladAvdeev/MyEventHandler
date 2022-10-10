a24719f8d4fdd63a8285e4a752d35e6a1a684998
VladAvdeev <vladislav.avdeev55@gmail.com>
2022-10-10 22:53:46 +0300

    Добавьте файлы проекта.

---
+++ MyEventHandler/AsyncCaller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyEventHandler
+{
+    internal class AsyncCaller
+    {
+        private readonly EventHandler eventHandler;ы
+
+        public AsyncCaller(EventHandler eventHandler)
+        {
+            this.eventHandler = eventHandler;
+        }
+        public bool Invoke(int timeout, object sender, EventArgs e)
+        {
+            Thread thread = new Thread(() => { eventHandler.Invoke(sender, e); });
+            thread.Start();
+            thread.Join();
+            return thread.Join(timeout);
+        }
+    }
+}
---
+++ MyEventHandler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace MyEventHandler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+        static void MyEventHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Handler statrs");
+            Thread.Sleep(5000);
+            Console.WriteLine("Handler ends");
+        }
+        void Run()
+        {
+            EventHandler h = new EventHandler(MyEventHandler);
+            AsyncCaller ac = new AsyncCaller(h);
+            bool completedOK = ac.Invoke(5000, null, EventArgs.Empty);
+            Console.WriteLine($"completedOK: {completedOK.ToString()}");
+            bool completedOK2 = ac.Invoke(2000, null, EventArgs.Empty);
+            Console.WriteLine($"completedOK2: {completedOK2.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
